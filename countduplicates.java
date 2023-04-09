23cae8a63d13ab3063d9419f5e3a04071042e032
Akshay <akshay0127.be21@chitkara.edu.in>
2023-04-09 13:26:31 +0530

    Added Count Duplicates Program in Java

---
+++ countduplicates.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+
+public class countduplicates {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.println("Enter The Original Number");
+        int number = input.nextInt();
+        System.out.println("Enter The Number Whose No of Occurrence You Want to Know ");
+        int n = input.nextInt();
+        int count = 0;
+        while (number != 0) {
+            int remainder = number % 10;
+
+            if (n == remainder) {
+                count++;
+
+            }
+            number = number / 10;
+
+        }
+        System.out.println("The Number of Occurence of " +" " + n + " is " + count);
+
+        
+    }
+
+    
+}
