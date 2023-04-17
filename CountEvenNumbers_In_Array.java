494dedbc7751b3482a7f64f8b088fce7116efa69
Akshay <akshay0127.be21@chitkara.edu.in>
2023-04-17 10:32:48 +0530

    Added a Program in java that can count the number of even elements in an array

---
+++ CountEvenNumbers_In_Array.java
@@ -0,0 +1,35 @@
+public class CountEvenNumbers_In_Array {
+
+    static boolean even(int num) {
+        int digits = noofdigits(num);
+        if (digits % 2 == 0) {
+            return true;
+        }
+        return false;
+    }
+
+    static int noofdigits(int num) {
+        int count = 0;
+        while (num > 0) {
+            count++;
+            num = num / 10;
+        }
+        return count;
+    }
+
+    static int findEvendigits(int arr[]) {
+        int count = 0;
+        for (int i = 0; i < arr.length; i++) {
+            if (even(arr[i])) {
+                count++;
+            }
+        }
+        return count;
+    }
+
+    public static void main(String[] args) {
+        int arr[] = { 123, 786, 4586, 78 };
+        int ans = findEvendigits(arr);
+        System.out.println(ans);
+    }
+}
