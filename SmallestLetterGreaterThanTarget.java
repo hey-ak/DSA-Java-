5ff3fe899994a34e031d00f25aa631d39eb835a4
Akshay <akshay0127.be21@chitkara.edu.in>
2023-04-18 14:04:04 +0530

    Added Smallest Letter greater than target problem in java

---
+++ SmallestLetterGreaterThanTarget.java
@@ -0,0 +1,24 @@
+public class SmallestLetterGreaterThanTarget {
+    public static void main(String[] args) {
+        char[] letters = {'a', 'b', 'c', 'e', 'f', 'h'};
+        char target = 'd';
+        char result = nextGreatestLetter(letters, target);
+        System.out.println(result);
+    }
+    
+    public static char nextGreatestLetter(char[] letters, char target) {
+        int start = 0;
+        int end = letters.length - 1;
+        
+        while (start <= end) {
+            int mid = start + (end - start) / 2;
+            if (letters[mid] > target) {
+                end = mid - 1;
+            } else {
+                start = mid + 1;
+            }
+        }
+        
+        return letters[start % letters.length];
+    }
+}
