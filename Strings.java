e64dd27871b4cb92113b902654065138aa3612fd
Akshay <akshay0127.be21@chitkara.edu.in>
2023-05-15 12:30:15 +0530

    Added STrings and String Builder in Java

---
+++ Strings.java
@@ -0,0 +1,17 @@
+public class Strings {
+    public static void main(String[] args) {
+        String a = "Hello";
+        String b = "Hello";
+
+        String c = new String("Hello");
+        String d = new String("Hello");
+
+        // System.out.println(a == b);// Comperator
+
+        // System.out.println(c.equals(d)); // Method 
+
+        // System.out.println(c==d);
+        
+    }
+    
+}
---
+++ prettyprinting.java
@@ -0,0 +1,18 @@
+
+import java.util.*;
+
+public class prettyprinting {
+    public static void main(String[] args) {
+        float a = 456.127f;
+
+        // System.out.printf("value till 2 decimal digits is %.2f", a);
+        
+        float pie = (float) Math.PI;
+        System.out.printf("Pie: %.3f", pie);
+
+        System.out.printf("Hello My name is %s and I am %s", "Akshay", "Programmer");
+      
+
+
+    }
+}
